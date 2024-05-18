3a4040b9dd97393635cb79b22c8d75d714d61ac5
Erkin Arıcı <erkin28465@hotmail.com>
2024-05-19 00:14:01 +0300

    fixes spawn location of the enemy

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -12,13 +12,27 @@ public class EnemySpawner : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        GameObject enemyHomelessClone = Instantiate(enemyHomelessPrefab, spawnPosition, Quaternion.identity);
-        // Get the EnemyHomeless component of the clone
-        EnemyHomeless enemyHomeless = enemyHomelessClone.GetComponent<EnemyHomeless>();
-
-        if (enemyHomeless != null)
+        // Get the player's position
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
         {
-            enemyHomeless.coinPrefab = coinPrefab;
+            Vector3 playerPosition = player.transform.position;
+
+            // Define the scene bounds
+            float minX = -10.0f; // Adjust these values as needed
+            float maxX = 25.0f;
+
+            // Calculate the furthest position from the player
+            Vector3 spawnPosition = playerPosition.x > 0 ? new Vector3(minX, -3.5f, 0) : new Vector3(maxX - 5, -3.5f, 0);
+
+
+            GameObject enemyHomelessClone = Instantiate(enemyHomelessPrefab, spawnPosition, Quaternion.identity);
+            // Get the EnemyHomeless component of the clone
+            EnemyHomeless enemyHomeless = enemyHomelessClone.GetComponent<EnemyHomeless>();
+            if (enemyHomeless != null)
+            {
+                enemyHomeless.coinPrefab = coinPrefab;
+            }
         }
     }
 
